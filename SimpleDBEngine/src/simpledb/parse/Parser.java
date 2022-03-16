7ec3112ab2bb243b946ec9d19f5f4e0fb1d5ef9e
Jiaxiang Yu <56266729+litone01@users.noreply.github.com>
2022-03-16 15:32:25 +0800

    [week 6- group by-continue] Fix group by (#19)
    * change order of aggregate/groupby clause to be before projection and add existing schmea in group plan
    
    * add aggregate function's field names in query data when agg fns are used, rather than the plain field name. eg. countofsid is added, rather than sid. just adding sid will cause field name cannot be found
    
    * remove adding schema in group by plan

--- SimpleDBEngine/src/simpledb/parse/Parser.java
+++ SimpleDBEngine/src/simpledb/parse/Parser.java
@@ -42,7 +42,6 @@ public class Parser {
    }
    
    public Term term() {
-	   //modify for lab 1
       Expression lhs = expression();
       Operator opr = lex.eatOpr();
       Expression rhs = expression();
@@ -74,7 +73,6 @@ public class Parser {
    
    public QueryData query() {
       lex.eatKeyword("select");
-//      List<String> fields = selectList();
       List<String> fields = new ArrayList<>();
       List<AggregationFn> aggregationFns = new ArrayList<>();
 
@@ -106,18 +104,17 @@ public class Parser {
             }
 
             aggregationFns.add(aggregationFn);
+            fields.add(aggregationFn.fieldName());
          } else {
             field = field();
-//            fields.add(field);
+            fields.add(field);
          }
-         fields.add(field);
+         
          if(!lex.matchDelim(',')){
             break;
          }
          lex.eatDelim(',');
       }
-
-//      List<String> fields = selectList();
 
       lex.eatKeyword("from");
       Collection<String> tables = tableList();
